be826e47594308e0765e89eadf60e4cfde232d2a
Madalin Popa <contact@madalinpopa.com>
2019-04-11 07:47:16 +0300

    adding colors

--- windows/powershell.ps1
+++ windows/powershell.ps1
@@ -18,3 +18,37 @@ Function touch
 
 # change the color of the error
 $host.PrivateData.ErrorForegroundColor = 'White'
+
+
+
+# similar touch function in powershell
+Function touch
+{
+    $file = $args[0]
+    if($file -eq $null) {
+        throw "No filename supplied"
+    }
+
+    if(Test-Path $file)
+    {
+        (Get-ChildItem $file).LastWriteTime = Get-Date
+    }
+    else
+    {
+        echo $null > $file
+    }
+}
+
+# change the color of the error
+$host.PrivateData.ErrorForegroundColor = 'White'
+
+# Set-PSReadlineOption -TokenKind comment -ForegroundColor white
+# Set-PSReadlineOption -TokenKind none -ForegroundColor white
+Set-PSReadlineOption -TokenKind command -ForegroundColor white
+# Set-PSReadlineOption -TokenKind parameter -ForegroundColor white
+# Set-PSReadlineOption -TokenKind variable -ForegroundColor white
+# Set-PSReadlineOption -TokenKind type -ForegroundColor white
+# Set-PSReadlineOption -TokenKind number -ForegroundColor white
+# Set-PSReadlineOption -TokenKind string -ForegroundColor white
+# Set-PSReadlineOption -TokenKind operator -ForegroundColor white
+# Set-PSReadlineOption -TokenKind member -ForegroundColor white
